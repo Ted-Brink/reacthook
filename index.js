4d33c5e0cc0c57ff21ecfa777e70a62aeb7cfeca
Ted-Brink <84717088+Ted-Brink@users.noreply.github.com>
2021-11-15 13:35:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import reactDom from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
-ReactDOM.render(<div>Hello World</div>, document.getElementById("hello"));
+ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
